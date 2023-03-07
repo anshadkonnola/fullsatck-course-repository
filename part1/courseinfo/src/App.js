2eb76c0638c1ac4a2cac45c94f8146cc9aea82ac
anshadkonnola <ae20b035@smail.iitm.ac.in>
2023-03-07 10:36:52 +0530

    feat: part1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const Header = (props) => (
+    <h1>{props.name}</h1>
+)
+
+const Part = (props) => (
+    <>
+      <h2>{props.part.name}</h2>
+      <p>There are <u><b>{props.part.exercises}</b></u> exercises in this part of the course.</p>
+    </>
+)
+
+const Content = (props) => (
+    <>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </>
+)
+
+const Total = (props) => {
+  let total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
+  // props.parts.forEach(part => {
+  //   total += part.exercises;
+  // })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <p>There are <u><b>{total}</b></u> exercises in total.</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  )
 }
 
 export default App;
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const Heading = ({ header }) => <h2>{header}</h2>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const StatisticLine = ({ text, value }) => {
+  if (text === 'positive') {
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+      )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  let count =  good + neutral + bad
+  let average = (good - bad) / count
+  let positive = (good / count) * 100
+
+  if (count === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={count} />
+        <StatisticLine text='average' value={average} />
+        <StatisticLine text='positive' value={positive} />
+      </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = (good) => setGood(good)
+  const setToNeutral = (neutral) => setNeutral(neutral)
+  const setToBad = (bad) => setBad(bad)
+
+  return (
+    <div>
+      <Heading header='give feedback' />
+      <Button handleClick={ () => setToGood(good + 1) } text='good' />
+      <Button handleClick={ () => setToNeutral(neutral + 1) } text='neutral' />
+      <Button handleClick={ () => setToBad(bad + 1) } text='bad' />
+
+      <Heading header='statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
