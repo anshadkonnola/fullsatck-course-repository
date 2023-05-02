c7b9949510c6a0f39ddbdb0f3e6e8924cfd3c669
Mohammed Anshad <85294714+anshadkonnola@users.noreply.github.com>
2023-05-02 11:11:09 +0530

    feat: added database to delete and update method

--- part3/course/index.js
+++ part3/course/index.js
@@ -12,14 +12,11 @@ const requestLogger = (request, response, next) => {
     next()
 };
   
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-};
 
-app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
 app.use(requestLogger);
-app.use(express.static('build'));
+
 
 let notes = [];
 
@@ -34,18 +31,22 @@ app.get('/api/notes', (request, response) => {
     })
 });
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note);
+        if(note) response.json(note);
+        else response.status(404).end();
+    })
+    .catch(error => {
+        next(error);
     })
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-    console.log('deleted');
-
-    response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
 app.post('/api/notes', (request, response) => {
@@ -68,21 +69,37 @@ app.post('/api/notes', (request, response) => {
 });
 
 app.put('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
     const body = request.body;
 
-    const updatedNote = {
+    const note = {
         content: body.content,
-        important: body.important,
-        id: generateId()
-    };
+        important: body.important
+    }
 
-    notes = notes.map(note => note.id !== id ? note : updatedNote);
-
-    response.json(updatedNote);
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote);
+        })
+        .catch(error => next(error));
 });
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+};
+
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+app.use(errorHandler)
 
 
 
