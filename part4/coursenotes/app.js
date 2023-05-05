ddc4fa316bfe899c3d530b37bc1fc0020e0a5a31
Mohammed Anshad <85294714+anshadkonnola@users.noreply.github.com>
2023-05-05 14:42:56 +0530

    feat: restructuring the app to best practice

--- part4/coursenotes/app.js
+++ part4/coursenotes/app.js
@@ -1,103 +1,32 @@
-// eslint-disable-next-line no-undef
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
-const Note = require('./models/note')
+const cors = require('cors')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
+mongoose.set('strictQuery', false)
 
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
+app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(requestLogger)
+app.use(middleware.requestLogger)
 
-app.get('/', (request, response) => {
-    response.sendFile('./build/index.html') //send
-})
+app.use('/api/notes', notesRouter)
 
-app.get('/api/notes', (request, response) => {
-    Note.find({}).then((notes) => {
-        response.json(notes)
-    })
-})
-
-app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then((note) => {
-            if (note) response.json(note)
-            else response.status(404).end()
-        })
-        .catch((error) => {
-            next(error)
-        })
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(() => {
-            response.status(204).end()
-        })
-        .catch((error) => next(error))
-})
-
-app.post('/api/notes', (request, response, next) => {
-    const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing',
-        })
-    }
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-    })
-
-    note
-        .save()
-        .then((savedNote) => {
-            response.json(savedNote)
-        })
-        .catch((error) => next(error))
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-    const { content, important } = request.body
-
-    Note.findByIdAndUpdate(
-        request.params.id,
-        { content, important },
-        { new: true, runValidators: true, context: 'query' }
-    )
-        .then((updatedNote) => {
-            response.json(updatedNote)
-        })
-        .catch((error) => next(error))
-})
-
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-
-    next(error)
-}
-
-app.use(errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
